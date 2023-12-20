a86cc156e51da431bbcdb5c8a00dae33a1b40b83
Dmitriy <mdmgrand@mail.ru>
2023-12-20 21:42:22 +0300

    add comment photolistview

--- conf/api/urls.py
+++ conf/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import PhotosListView, Info, Random
+from api.views import Photo, Info, Random, PhotoListView, CommentListView
 
 urlpatterns = [
-    path("photos/", PhotosListView.as_view()),
+    path("photos/", PhotoListView.as_view()),
+    path("comments/", CommentListView.as_view()),
     path("info/", Info.as_view()),
     path("random/", Random.as_view()),
     ]
--- conf/api/views.py
+++ conf/api/views.py
@@ -2,21 +2,63 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import random
 
-from blog.models import Photo
+from blog.models import Photo, Comment
 
 
 # Create your views here.
-class PhotosListView(APIView):
+class PhotoListView(APIView):
     def get(self, request):
         photos = Photo.objects.all().values()
         return Response({"photos": photos})
 
+    def post(self, request):
+        name_r = request.data["name"]
+        description_r = request.data["description"]
+        image_r = request.data["image"]
+        author_r = request.data["author"]
+        photo = Photo.objects.create(
+            name=name_r,
+            description=description_r,
+            image=image_r,
+            author_id=author_r
+        )
+
+        return Response({
+            'name': name_r,
+            'description': description_r,
+            'image': str(image_r),
+            'author_id': author_r
+        })
+
+class CommentListView(APIView):
+    def get(self, request):
+        comments = Comment.objects.all().values()
+        return Response({"comments": comments})
+
+    def post(self, request):
+        text_comment_r = request.data["text_comment"]
+        author_comment_r = request.data["author_comment"]
+        image_comment_r = request.data["image_comment"]
+
+        comment = Comment.objects.create(
+            text_comment=text_comment_r,
+            author_comment=author_comment_r,
+            image_comment_id=image_comment_r,
+        )
+
+        return Response({
+            'text_comment': text_comment_r,
+            'author_comment': author_comment_r,
+            'image_comment_id': image_comment_r
+        })
+
 class Info(APIView):
     def get(self, request):
-        return Response({"name": "Петя","jjj": "Петя","last_name": "Иванов",
+        return Response({"name": "Петя", "last_name": "Иванов",
                          "age": 20})
+
+
 class Random(APIView):
     def get(self, request):
-        rand = random.randint(1,100)
+        rand = random.randint(1, 100)
         return Response({"number": rand})
-
